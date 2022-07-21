f76440f93d044af6ad1f6f3470f9f9c2a263abe1
shalini-c <shalinichaudhary@gmail.com>
2022-07-21 09:01:54 -0400

    include function to fetch and store image from url - commit message from prash

--- vision_tester.py
+++ vision_tester.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
 
-print("Hello World")
+import tensorflow as tf
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_image_from_url(image_url):
+  # If the temporary test_image.jpg file already exists, 
+  # delete it so a new one can be made.
+  if os.path.exists('/root/.keras/datasets/test_image.jpg'):
+    os.remove('/root/.keras/datasets/test_image.jpg')
+
+  image_path = tf.keras.utils.get_file('test_image.jpg', origin=image_url)
+  return image_path
